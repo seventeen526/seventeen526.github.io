ba6de9f7283750e8c207af0771df588f230efa11
seventeen526 <1507828837@qq.com>
2025-08-08 11:22:16 +0800

    Site updated: 2025-08-08 11:22:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/28/hello-world/","2025/07/29/test1/","2025/07/29/Centos8 Apache的搭建/","2025/07/29/linux/"];function toRandomPost(){
+var posts=["2025/07/28/hello-world/","2025/07/29/test1/","2025/07/29/Centos8 Apache的搭建/","2025/07/29/linux/","2025/07/29/VIM编辑器和用户组管理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
