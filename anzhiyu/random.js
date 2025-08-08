3b1325ba4ad3ff952ba24db52b861b466903a421
seventeen526 <1507828837@qq.com>
2025-08-08 12:42:44 +0800

    Site updated: 2025-08-08 12:42:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/28/hello-world/","2025/07/29/test1/","2025/07/29/Centos8 Apache的搭建/","2025/07/29/linux/","2025/07/29/VIM编辑器和用户组管理/"];function toRandomPost(){
+var posts=["2025/07/28/hello-world/","2025/07/29/test1/","2025/07/29/Centos8 Apache的搭建/","2025/07/29/linux/","2025/07/29/VIM编辑器和用户组管理/","2025/07/29/linux基础大作业w1/","2025/07/29/压缩和解压缩/","2025/07/29/提权/","2025/07/29/用户权限/","2025/07/29/软件管理/","2025/07/29/重定向/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
